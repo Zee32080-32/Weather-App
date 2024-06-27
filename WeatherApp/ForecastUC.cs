3288a4aaf7425dd30f14841099ffe26d3ce4abcf
Zee32080-32 <132150392+Zee32080-32@users.noreply.github.com>
2024-06-28 00:51:39 +0100

    Add files via upload

---
+++ WeatherApp/ForecastUC.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WeatherApp
+{
+    public partial class ForecastUC : UserControl
+    {
+        public ForecastUC()
+        {
+            InitializeComponent();
+        }
+
+        private void ForecastUC_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
